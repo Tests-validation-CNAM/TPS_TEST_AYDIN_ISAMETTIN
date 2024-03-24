2e15a9955b2c203714ed0a94445b2dc420af2ef6
AIsamet <isamet2067@gmail.com>
2024-03-24 13:47:08 +0100

    added class Mensualite and tests

---
+++ TP03/TP03/TP03/Mensualite.cs
@@ -0,0 +1,15 @@
+﻿namespace TP03;
+
+public class Mensualite
+{
+    public int Numero { get; private set; }
+    public double MontantPaye { get; private set; }
+    public double ResteAPayer { get; private set; }
+
+    public Mensualite(int numero, double montantPaye, double resteAPayer)
+    {
+        this.Numero = numero;
+        this.MontantPaye = montantPaye;
+        this.ResteAPayer = resteAPayer;
+    }
+}
---
+++ TP03/TP03/TP03Test/MensualiteTest.cs
@@ -0,0 +1,17 @@
+﻿namespace TP03;
+
+public class MensualiteTest
+{
+    [Theory]
+    [InlineData(1, 100, 1000)]
+    [InlineData(2, 200, 800)]
+    [InlineData(3, 300, 500)]
+    public void TestMensualite(int numero, double montantPaye, double resteAPayer)
+    {
+        Mensualite mensualite = new Mensualite(numero, montantPaye, resteAPayer);
+
+        Assert.Equal(numero, mensualite.Numero);
+        Assert.Equal(montantPaye, mensualite.MontantPaye);
+        Assert.Equal(resteAPayer, mensualite.ResteAPayer);
+    }
+}
