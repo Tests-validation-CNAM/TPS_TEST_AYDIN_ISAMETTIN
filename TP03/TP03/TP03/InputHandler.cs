d558e2629c559649d4aea7d3a8895eae833dd9ee
AIsamet <isamet2067@gmail.com>
2024-03-24 12:57:16 +0100

    added tests and code for inputs check

--- TP03/TP03/TP03/InputHandler.cs
+++ TP03/TP03/TP03/InputHandler.cs
@@ -2,15 +2,73 @@
 
 public class InputHandler
 {
-    private double[] args;
+    private string[] args;
+    private const double MONTANT_MIN = 50000;
+    private const double DUREE_MIN_MOIS = 9 * 12;
+    private const double DUREE_MAX_MOIS = 25 * 12;
+    private const double TAUX_MIN = 0;
+    private const double TAUX_MAX = 100;
 
-    public InputHandler(double[] args)
+    public InputHandler(string[] args)
     {
         this.args = args;
     }
 
-    public double getMontant()
+    public double InputMontant()
     {
-        return this.args[0];
+        double montant = GetInput(0);
+
+        if (montant < MONTANT_MIN)
+        {
+            throw new ArgumentException("Le montant doit être d'au moins " + MONTANT_MIN + "€");
+        }
+
+        return montant;
+    }
+
+    public int InputDuree()
+    {
+        int duree = (int)GetInput(1);
+
+        if (duree < DUREE_MIN_MOIS || duree > DUREE_MAX_MOIS)
+        {
+            throw new ArgumentException("La durée doit être comprise entre " + DUREE_MIN_MOIS + " et " + DUREE_MAX_MOIS + " mois");
+        }
+
+        return duree;
+    }
+
+    public double InputTaux()
+    {
+        double taux = GetInput(2);
+
+        if (taux <= TAUX_MIN || taux > TAUX_MAX)
+        {
+            throw new ArgumentException("Le taux d'intérêt annuel doit être compris entre 0 et 100%");
+        }
+
+        return taux;
+    }
+
+    private double GetInput(int index)
+    {
+        string input = args[index];
+
+        if (string.IsNullOrEmpty(input))
+        {
+            throw new ArgumentNullException();
+        }
+
+        if (!double.TryParse(input, out double value))
+        {
+            throw new FormatException();
+        }
+
+        if (value < 0)
+        {
+            throw new ArgumentOutOfRangeException();
+        }
+
+        return value;
     }
 }
--- TP03/TP03/TP03Test/InputHandlerTest.cs
+++ TP03/TP03/TP03Test/InputHandlerTest.cs
@@ -2,17 +2,170 @@ namespace TP03;
 
 public class InputHandlerTest
 {
-    [Fact]
-    public void TestGetMontant()
+    // inline data
+    [Theory]
+    [InlineData("300000", 300000)]
+    [InlineData("50000", 50000)]
+    [InlineData("  100000  ", 100000)]
+    public void TestInputMontant_ValidMontant(string input, double result)
     {
         // Arrange
-        double[] args = new double[] { 300000 };
+        string[] args = { input };
         InputHandler inputHandler = new InputHandler(args);
 
         // Act
-        double montant = inputHandler.getMontant();
+        double montant = inputHandler.InputMontant();
 
         // Assert
-        Assert.Equal(300000, montant);
+        Assert.Equal(result, montant);
+    }
+
+    [Fact]
+    public void TestInputMontant_InvalidStringFormat()
+    {
+        // Arrange
+        string[] args = { "InvalidInput" };
+        InputHandler inputHandler = new InputHandler(args);
+
+        // Act
+        Action Action = () => inputHandler.InputMontant();
+
+        // Assert
+        var exception = Assert.Throws<FormatException>(Action);
+        Assert.Equal("One of the identified items was in an invalid format.", exception.Message);
+    }
+
+    [Fact]
+    public void TestInputMontant_InvalidMontantMin()
+    {
+        // Arrange
+        string[] args = { "49000" };
+        InputHandler inputHandler = new InputHandler(args);
+
+        // Act
+        Action Action = () => inputHandler.InputMontant();
+
+        // Assert
+        var exception = Assert.Throws<ArgumentException>(Action);
+        Assert.Equal("Le montant doit être d'au moins 50000€", exception.Message);
+    }
+
+    [Fact]
+    public void TestInputMontant_InvalidMontantNegatif()
+    {
+        // Arrange
+        string[] args = { "-50000" };
+        InputHandler inputHandler = new InputHandler(args);
+
+        // Act
+        Action Action = () => inputHandler.InputMontant();
+
+        // Assert
+        var exception = Assert.Throws<ArgumentOutOfRangeException>(Action);
+        Assert.Equal("Specified argument was out of the range of valid values.", exception.Message);
+    }
+
+    [Fact]
+    public void TestInputMontant_InvalidMontantNull()
+    {
+        // Arrange
+        string[] args = { null };
+        InputHandler inputHandler = new InputHandler(args);
+
+        // Act
+        Action action = () => inputHandler.InputMontant();
+
+        // Assert
+        var exception = Assert.Throws<ArgumentNullException>(action);
+        Assert.Equal("Value cannot be null.", exception.Message);
+    }
+
+    [Fact]
+    public void TestInputDuree_ValidDuree()
+    {
+        // Arrange
+        string[] args = { "50000", "108" };
+        InputHandler InputHandler = new InputHandler(args);
+
+        // Act
+        int duree = InputHandler.InputDuree();
+
+        // Assert
+        Assert.Equal(108, duree);
+    }
+
+    [Fact]
+    public void TestInputDuree_InvalidStringFormat()
+    {
+        // Arrange
+        string[] args = { "50000", "InvalidInput" };
+        InputHandler InputHandler = new InputHandler(args);
+
+        // Act
+        Action action = () => InputHandler.InputDuree();
+
+        // Assert
+        var exception = Assert.Throws<FormatException>(action);
+        Assert.Equal("One of the identified items was in an invalid format.", exception.Message);
+    }
+
+    [Fact]
+    public void TestInputDuree_InvalidDuree()
+    {
+        // Arrange
+        string[] args = { "50000", "107" };
+        InputHandler InputHandler = new InputHandler(args);
+
+        // Act
+        Action action = () => InputHandler.InputDuree();
+
+        // Assert
+        var exception = Assert.Throws<ArgumentException>(action);
+        Assert.Equal("La durée doit être comprise entre 108 et 300 mois", exception.Message);
+    }
+
+    [Fact]
+    public void TestInputDuree_InvalidDureeNull()
+    {
+        // Arrange
+        string[] args = { "50000", null };
+        InputHandler InputHandler = new InputHandler(args);
+
+        // Act
+        Action action = () => InputHandler.InputDuree();
+
+        // Assert
+        var exception = Assert.Throws<ArgumentNullException>(action);
+        Assert.Equal("Value cannot be null.", exception.Message);
+    }
+
+
+    [Fact]
+    public void TestInputTaux_ValidTaux()
+    {
+        // Arrange
+        string[] args = { "50000", "240", "3,5" };
+        InputHandler InputHandler = new InputHandler(args);
+
+        // Act
+        double taux = InputHandler.InputTaux();
+
+        // Assert
+        Assert.Equal(3.5, taux);
+    }
+
+    [Fact]
+    public void TestInputTaux_InvalidTaux()
+    {
+        // Arrange
+        string[] args = { "50000", "240", "122" };
+        InputHandler InputHandler = new InputHandler(args);
+
+        // Act
+        Action action = () => InputHandler.InputTaux();
+
+        // Assert
+        var exception = Assert.Throws<ArgumentException>(action);
+        Assert.Equal("Le taux d'intérêt annuel doit être compris entre 0 et 100%", exception.Message);
     }
 }
